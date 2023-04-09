224443cec793b2244ccf7cf39984a80c94428f72
Ivan Meshkov <obdultipov@yandex.ru>
2023-04-09 19:23:53 +0600

    RecipeviewSet queryset edited

--- backend/api/views.py
+++ backend/api/views.py
@@ -97,7 +97,7 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = (
-        Recipe.objects.select_related('author').prefetch_related('tags').all()
+        Recipe.objects.select_related('author').prefetch_related('tags')
     )
     serializer_class = RecipeGetSerializer
     filter_backends = (DjangoFilterBackend,)
